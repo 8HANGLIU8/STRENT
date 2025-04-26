685ecdff0cffc05b9e52cae6403f9a3aa13f1471
FelixSouan <felixsouan@gmail.com>
2025-04-26 19:10:41 -0400

    Add Tailwind CSS setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">Hello, Strent!</h1>
+      </div>
     </div>
   );
 }
