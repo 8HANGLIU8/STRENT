db281a8fbe478d4add4696562904fc164ad95604
8HANGLIU8 <8.h.a.n.g.l.i.u.8@gmail.com>
2025-04-26 22:07:26 -0400

    error fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-<<<<<<< HEAD
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
-import Error404 from './pages/ErrorPage';
-=======
-import Home from './pages/Home'
-import Welcome from './pages/Welcome';
-import Login from './pages/login';
-import SignUp from './pages/signup';
-import Navbar from './components/Navbar'; // Add this import
-import Error404 from './pages/error404';
->>>>>>> 8c3ecb75b7d30d42ee7320c8de84e8d6d917af27
+import Login from './pages/login.js';
+import SignUp from './pages/signup.js';
+import Error404 from './pages/error404.js';
+import Home from './pages/Home.js';
+import Welcome from './pages/Welcome.js';
 
 function App() {
   return (
@@ -21,15 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
         <Route path="/" element={<Navigate to="/" />} /> {/* redirect */}
+        <Route path="/Welcome" element={<Welcome />} />
         <Route path="*" element={<Error404 />} />
-=======
-        <Route path="/welcome" element={< Welcome/>} />
-        <Route path="/404" element={< Error404/>} />
-        <Route path="*" element={<Navigate to="/404" />} />
-        
->>>>>>> 8c3ecb75b7d30d42ee7320c8de84e8d6d917af27
       </Routes>
     </Router>
   );
